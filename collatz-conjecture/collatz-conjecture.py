85197f8b7ae4afe64755fdc2c28680e334a347b4
noClaps <noClaps@users.noreply.github.com>
2022-07-15 21:58:57 +0000

    deploy: 68169debfa8a32205cadf5252084af7cf9171ae7

--- collatz-conjecture/collatz-conjecture.py
+++ collatz-conjecture/collatz-conjecture.py
@@ -1,11 +1,11 @@
-def collatz(n): # making the collatz function
-    while n != 1: # this tells the code to keep running until it reaches 1
-        if n % 2: # if the number is odd, run code below
+def collatz(n): # make collatz function
+    while n != 1: # tell code to keep running until it reaches 1
+        if n % 2: # if number is odd, multiply by 3 and add 1
             n = n*3 + 1
-        else: # else, if the number is even, run code below
+        else: # else, if number is even, halve it
             n = n / 2
         print(n) # output number onto screen
 
-num = int(input("Enter a number to test: ")) # this allows you to put in a different number while running the code instead of having to change the code each time
+num = int(input("Enter a number to test: ")) # ask user for number to test
 collatz(num) # run the collatz function with the number you type in
-input() # this is there to let you read the output, but otherwise useless to the code
+input() # Press enter to exit
