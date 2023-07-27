0073b358fc85fe49a560557577e8c9aac2b56f39
noClaps <github@zerolimits.dev>
2023-07-27 17:48:10 +0100

    Add photos docs

--- sites/gallery/astro.config.mjs
+++ sites/gallery/astro.config.mjs
@@ -1,18 +1,18 @@
-import {defineConfig, sharpImageService} from "astro/config";
+import { defineConfig, sharpImageService } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://gallery.zerolimits.dev",
-    integrations: [sitemap()],
-    experimental: {
-        assets: true,
-    },
-    image: {
-        service: sharpImageService(),
-    },
-    compressHTML: true,
-    build: {
-        inlineStylesheets: "auto"
-    }
+  site: "https://gallery.zerolimits.dev",
+  integrations: [ sitemap() ],
+  experimental: {
+    assets: true,
+  },
+  image: {
+    service: sharpImageService(),
+  },
+  compressHTML: true,
+  build: {
+    inlineStylesheets: "auto"
+  }
 });
