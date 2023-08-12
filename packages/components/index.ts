a2b5091076d427d618a8a606192124ff61c4e380
noClaps <github@zerolimits.dev>
2023-08-13 03:44:30 +0800

    Add GlobalNav to index.ts

--- packages/components/index.ts
+++ packages/components/index.ts
@@ -5,3 +5,4 @@ export { default as Footer } from "./src/Footer.astro";
 export { default as Page404 } from "./src/404.astro";
 export { default as Sidebar } from "./src/Sidebar.astro";
 export { default as Layout } from "./src/Layout.astro";
+export { default as GlobalNav } from "./src/GlobalNav.astro";
