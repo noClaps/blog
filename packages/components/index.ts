1b8997b3887f4751722ed4c640ba4b614dc2f0cd
noClaps <github@zerolimits.dev>
2023-08-13 04:26:57 +0800

    Create a common layout for all ZeroLimits.dev websites (#65)
    * Create global nav component
    
    * Add GlobalNav to index.ts
    
    * Use new GlobalNav in blog header
    
    * Remove blog about page
    
    * Use GlobalNav in gallery header
    
    * Create global header and add nav to it
    
    * Use global header in gallery
    
    * Use global header in homepage
    
    The links field is set to an empty array because I'm not sure how to make the links prop optional without breaking everything all the way down.
    
    * Use global header for blog
    
    * Reorder components `src/` folder

--- packages/components/index.ts
+++ packages/components/index.ts
@@ -1,7 +1,3 @@
-export { default as ThemeToggle } from "./src/ThemeToggle.astro";
 export { default as Icon } from "./src/Icon.astro";
-export { default as Head } from "./src/Head.astro";
-export { default as Footer } from "./src/Footer.astro";
 export { default as Page404 } from "./src/404.astro";
-export { default as Sidebar } from "./src/Sidebar.astro";
 export { default as Layout } from "./src/Layout.astro";
