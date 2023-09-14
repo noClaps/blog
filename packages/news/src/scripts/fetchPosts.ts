57d13e435cf2bdfeb7b44ff3a7bd5d6a1e78d8d6
noClaps <github@zerolimits.dev>
2023-09-15 01:53:54 +0800

    Improve fetchPosts function

--- packages/news/src/scripts/fetchPosts.ts
+++ packages/news/src/scripts/fetchPosts.ts
@@ -20,10 +20,9 @@ export async function fetchPosts() {
         .then(r => r.json()) as number[];
 
     const posts = await Promise.all(
-        postIds.map(async (id) => {
-            return await fetch(`https://hacker-news.firebaseio.com/v0/item/${ id }.json`)
-                .then(r => r.json()) as PostData;
-        })
+        postIds.map((id) => fetch(`https://hacker-news.firebaseio.com/v0/item/${ id }.json`)
+            .then(r => r.json()) as Promise<PostData>
+        )
     );
 
     return filterPosts(posts);
@@ -32,8 +31,8 @@ export async function fetchPosts() {
 function filterPosts(posts: PostData[]) {
     let filteredPosts = posts
         // Filter out posts that don't have URLs
-        .filter(post => post.url)
-        // Filter out Twitter links
-        .filter(post => !post.url?.startsWith("https://twitter.com")) as FilteredPosts[];
+        .filter(post => post.url
+            // Filter out twitter posts
+            && !post.url.startsWith("https://twitter.com")) as FilteredPosts[];
     return filteredPosts;
 }
