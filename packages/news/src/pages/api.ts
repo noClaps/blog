cb0f34583bc93d934c409bb5f84cfc37ff9d86a1
noClaps <github@zerolimits.dev>
2023-09-15 03:39:03 +0800

    Write my own HN API

---
+++ packages/news/src/pages/api.ts
@@ -0,0 +1,36 @@
+import type { APIRoute } from "astro";
+
+interface PostData {
+    title: string;
+    url?: string;
+}
+
+interface FilteredPostData extends PostData {
+    url: string;
+}
+
+export const GET: APIRoute = async () => {
+    const postIds = await fetch("https://hacker-news.firebaseio.com/v0/newstories.json").then(r => r.json()) as number[];
+    const postPromises = postIds.map(async (id) => {
+        return await fetch(`https://hacker-news.firebaseio.com/v0/item/${ id }.json`).then(r => r.json());
+    }) as Promise<PostData>[];
+
+    const posts = await Promise.all(postPromises).then(p => filter(p));
+    const postsData = posts.map(post => {
+        return {
+            url: post.url,
+            title: post.title,
+            domain: new URL(post.url).hostname
+        };
+    });
+
+    return new Response(
+        JSON.stringify(postsData), { status: 200 }
+    );
+};
+
+const filter = (posts: PostData[]) => {
+    return posts.filter(post =>
+        post.url
+        && !(new URL(post.url).hostname === "twitter.com")) as FilteredPostData[];
+}
