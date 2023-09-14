1b2fdceb064390b16d69d612d5f6c812c9e96159
noClaps <github@zerolimits.dev>
2023-09-15 05:45:55 +0800

    News optimisation (#73)
    * Improve fetchPosts function
    
    * Inline PostGrid into index page
    
    * Use HTTP streaming to speed up fetching data
    
    * Remove functionsPerRoute option
    
    * Write my own HN API
    
    * Add semicolon
    
    * Apply changes suggested by @MesterMan03
    
    * Limit to loading first 100 posts
    
    * Fetch the 90 newest posts

---
+++ packages/news/src/pages/api.ts
@@ -0,0 +1,39 @@
+import type { APIRoute } from "astro";
+
+interface PostData {
+    title: string;
+    url?: string;
+}
+
+interface FilteredPostData extends PostData {
+    url: string;
+}
+
+export const GET: APIRoute = async () => {
+    const postIds = await fetch("https://hacker-news.firebaseio.com/v0/newstories.json")
+        .then(r => r.json()) as number[];
+    const postPromises = postIds
+        .slice(0, 90)
+        .map((id) => fetch(`https://hacker-news.firebaseio.com/v0/item/${ id }.json`)
+            .then(r => r.json())) as Promise<PostData>[];
+
+    const posts = await Promise.all(postPromises)
+        .then(p => filter(p));
+    const postsData = posts.map(post => {
+        return {
+            url: post.url,
+            title: post.title,
+            domain: new URL(post.url).hostname
+        };
+    });
+
+    return new Response(
+        JSON.stringify(postsData), { status: 200 }
+    );
+};
+
+function filter(posts: PostData[]) {
+    return posts.filter(post =>
+        post.url
+        && !(new URL(post.url).hostname === "twitter.com")) as FilteredPostData[];
+};
