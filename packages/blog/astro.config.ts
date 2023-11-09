1340a9328e80dff4190984b51d446f91d907ecda
noClaps <github@zerolimits.dev>
2023-11-09 04:20:22 +0000

    Reimplement heading links

--- packages/blog/astro.config.ts
+++ packages/blog/astro.config.ts
@@ -1,7 +1,5 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
-import rehypeAutolinkHeadings from "rehype-autolink-headings";
-import { rehypeHeadingIds } from "@astrojs/markdown-remark";
 import remarkToc from "remark-toc";
 
 // https://astro.build/config
@@ -9,10 +7,6 @@ export default defineConfig({
     site: "https://blog.zerolimits.dev",
     markdown: {
         remarkPlugins: [remarkToc],
-        rehypePlugins: [
-            rehypeHeadingIds,
-            [rehypeAutolinkHeadings, { behavior: "wrap" }],
-        ],
     },
     integrations: [mdx()]
 });
