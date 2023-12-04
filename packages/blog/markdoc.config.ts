a3e9c49e0af43050c60da375140aa6d3b5191ad1
noClaps <github@zerolimits.dev>
2023-12-04 23:17:56 +0000

    Move post components to their own directory

--- packages/blog/markdoc.config.ts
+++ packages/blog/markdoc.config.ts
@@ -4,32 +4,32 @@ import shiki from "@astrojs/markdoc/shiki";
 export default defineMarkdocConfig({
     tags: {
         quote: {
-            render: component("./src/components/Quote.astro"),
+            render: component("./src/components/posts/Quote.astro"),
             attributes: {
                 name: { type: String },
                 link: { type: String }
             }
         },
         note: {
-            render: component("./src/components/Note.astro"),
+            render: component("./src/components/posts/Note.astro"),
             attributes: {
                 title: { type: String }
             }
         },
         warning: {
-            render: component("./src/components/Warning.astro"),
+            render: component("./src/components/posts/Warning.astro"),
             attributes: {
                 title: { type: String }
             }
         },
         math: {
-            render: component("./src/components/Math.astro"),
+            render: component("./src/components/posts/Math.astro"),
             attributes: {
                 display: { type: String }
             }
         },
         toc: {
-            render: component("./src/components/TableOfContents.astro"),
+            render: component("./src/components/posts/TableOfContents.astro"),
             attributes: {
                 headings: { type: String }
             }
