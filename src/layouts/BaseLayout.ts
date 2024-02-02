64e68bc0acadc0788650ed64e15246775aa73bd2
noClaps <github@zerolimits.dev>
2024-02-02 15:59:50 +0000

    Fix description in head

--- src/layouts/BaseLayout.ts
+++ src/layouts/BaseLayout.ts
@@ -30,7 +30,7 @@ export default function BaseLayout(
 	<title>${title}</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta name="description" content=${description}>
+	<meta name="description" content="${description}">
 	<link href="https://api.fontshare.com/v2/css?f[]=switzer@2,1&display=swap" rel="stylesheet">
 	<link rel="alternate" href="/feed.json" type="application/json" title="Feed">
 	<link rel="stylesheet" href="/styles/global.css">
