e9cd413eb68b2bca80a0b4c4ce034855ddf5af03
noClaps <github@zerolimits.dev>
2024-04-04 11:50:46 +0100

    Add start script

---
+++ scripts/start.ts
@@ -0,0 +1,18 @@
+Bun.serve({
+  async fetch({ url }) {
+    const path = new URL(url).pathname;
+
+    if (path === "/") return new Response(Bun.file(`dist/index.html`));
+    if (path === "/notes")
+      return new Response(Bun.file(`dist/notes/index.html`));
+
+    if (await Bun.file(`dist${decodeURI(path)}`).exists()) {
+      return new Response(Bun.file(`dist${decodeURI(path)}`));
+    }
+
+    return new Response(Bun.file(`dist${path}.html`));
+  },
+  reusePort: true,
+});
+
+console.log("Server running on http://localhost:3000.");
