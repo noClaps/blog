b6354c7177748062709bf89c4ad3064810c4f268
noClaps <04plugs-bios@icloud.com>
2024-12-02 03:31:31 +0000

    refactor: Move post layout into posts.ts file

--- src/pages/posts.ts
+++ src/pages/posts.ts
@@ -1,6 +1,46 @@
 import { render } from "@noclaps/znak";
-import { codeTheme, getPosts } from "../../scripts/utils.ts";
-import Post from "../layouts/Post.ts";
+import { codeTheme, formatDate, getPosts, html } from "../../scripts/utils.ts";
+import { Head, Header } from "../components.ts";
+
+interface PostProps {
+  title: string;
+  description?: string;
+  date: Date;
+  lastmod?: Date;
+  children: string;
+}
+function Post(props: PostProps) {
+  const { title, description, date, lastmod, children } = props;
+  const pubDate = lastmod ?? date;
+
+  return html`
+    <!doctype html>
+    <html lang="en">
+      <head>
+        ${Head({
+          title: title,
+          description:
+            description ??
+            "A blog about the most random things you can think of.",
+        })}
+        <link rel="stylesheet" href="/styles/post.css" />
+      </head>
+      <body>
+        ${Header({ showTitle: true })}
+        <main>
+          <div class="post-header">
+            <time datetime="${pubDate.toISOString()}">
+              ${formatDate(pubDate)}
+            </time>
+            <h1>${title}</h1>
+            ${description && `<p class="description">${description}</p>`}
+          </div>
+          <article>${children}</article>
+        </main>
+      </body>
+    </html>
+  `;
+}
 
 export async function writePosts() {
   const posts = await getPosts();
