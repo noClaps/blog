535dab843f9424832085e84c350d3bf2f05166be
noClaps <git@zerolimits.dev>
2025-04-14 10:09:28 +0100

    chore: Use feed template instead of `encoding/xml` lib

--- src/pages/feed.go
+++ src/pages/feed.go
@@ -1,9 +1,10 @@
 package pages
 
 import (
-	"encoding/xml"
+	"bytes"
 	"fmt"
 	"html"
+	"text/template"
 	"time"
 
 	utils "github.com/noClaps/blog/src"
@@ -11,35 +12,18 @@ import (
 )
 
 func atomFeed(items []utils.Post, content map[string]string, lastUpdate time.Time) (string, error) {
-	type Link struct {
-		Rel  string `xml:"rel,attr"`
-		Href string `xml:"href,attr"`
-	}
-	type EntryContent struct {
-		Type    string `xml:"type,attr"`
-		Content string `xml:",innerxml"`
-	}
 	type Entry struct {
-		XMLName   xml.Name     `xml:"entry"`
-		Id        string       `xml:"id"`
-		Title     string       `xml:"title"`
-		Updated   string       `xml:"updated"`
-		Content   EntryContent `xml:"content"`
-		Link      Link         `xml:"link"`
-		Summary   string       `xml:"summary,omitempty"`
-		Published string       `xml:"published"`
+		Id        string
+		Title     string
+		Updated   string
+		Content   string
+		Link      string
+		Summary   string
+		Published string
 	}
 	type Feed struct {
-		XMLName    xml.Name `xml:"feed"`
-		XMLNs      string   `xml:"xmlns,attr"`
-		Id         string   `xml:"id"`
-		Title      string   `xml:"title"`
-		LastUpdate string   `xml:"updated"`
-		Link       Link     `xml:"link"`
-		Icon       string   `xml:"icon"`
-		Subtitle   string   `xml:"subtitle"`
-		Author     string   `xml:"author>name"`
-		Entries    []Entry  `xml:"feed"`
+		LastUpdate string
+		Entries    []Entry
 	}
 
 	atomEntries := []Entry{}
@@ -49,41 +33,32 @@ func atomFeed(items []utils.Post, content map[string]string, lastUpdate time.Tim
 			updated = item.LastMod
 		}
 		atomEntries = append(atomEntries, Entry{
-			Id:      fmt.Sprintf("https://blog.zerolimits.dev%s", item.Slug),
-			Title:   item.Title,
-			Updated: updated.Format(time.RFC3339),
-			Content: EntryContent{
-				Type:    "html",
-				Content: html.EscapeString(content[item.Slug]),
-			},
-			Link: Link{
-				Rel:  "self",
-				Href: fmt.Sprintf("https://blog.zerolimits.dev%s", item.Slug),
-			},
+			Id:        fmt.Sprintf("https://blog.zerolimits.dev%s", item.Slug),
+			Title:     item.Title,
+			Updated:   updated.Format(time.RFC3339),
+			Content:   html.EscapeString(content[item.Slug]),
+			Link:      fmt.Sprintf("https://blog.zerolimits.dev%s", item.Slug),
 			Summary:   item.Description,
 			Published: item.Date.Format(time.RFC3339),
 		})
 	}
 
 	feed := Feed{
-		XMLNs:      "http://www.w3.org/2005/Atom",
-		Id:         "https://blog.zerolimits.dev/",
-		Title:      "The Blog of Random",
 		LastUpdate: lastUpdate.Format(time.RFC3339),
-		Link: Link{
-			Rel:  "self",
-			Href: "https://blog.zerolimits.dev/feed.atom",
-		},
-		Icon:     "https://blog.zerolimits.dev/favicon.ico",
-		Subtitle: "A blog about the most random things you can think of.",
-		Author:   "noClaps",
-		Entries:  atomEntries,
+		Entries:    atomEntries,
 	}
-	xml, err := xml.Marshal(feed)
+	tmpl, err := template.New("feed.atom").ParseFiles("src/pages/feed.atom")
 	if err != nil {
 		return "", err
 	}
-	return string(xml), nil
+
+	out := bytes.Buffer{}
+	err = tmpl.Execute(&out, feed)
+	if err != nil {
+		return "", err
+	}
+
+	return out.String(), nil
 }
 
 func Feed() (string, error) {
